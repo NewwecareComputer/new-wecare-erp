232659691bb08c1068aa4471e7fa8dbd42c74d7f
NewwecareComputer <new.wecarecomputer@gmail.com>
2026-08-28 11:38:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.put('/api/data',(req,res)=>{
   }catch(e){ res.status(500).json({error:e.message}); }
 });
 
-app.get('*',(req,res)=>{
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(ROOT,'erp','index.html'));
 });
 
